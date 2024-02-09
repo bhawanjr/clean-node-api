3c7a3ac90a827d1820622dcbd3b2f57fbf28fafd
bhawanjr <bhawanjr@hotmail.com>
2024-02-09 02:24:18 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -2,6 +2,15 @@ import { describe, expect, it, vitest } from 'vitest'
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+vitest.mock('bcrypt', async () => {
+  return {
+    default: {
+      hash: () => 'hashed_value'
+    }
+  }
+})
+
+
 describe('BCrypt Adapter', () => {
   it('Should call bcrypt with correct values', async () => {
     const salt = 12
@@ -9,5 +18,12 @@ describe('BCrypt Adapter', () => {
     const hashSpy = vitest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('Should returns a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hashed_value')
   })
 })
