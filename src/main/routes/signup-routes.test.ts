35a15d3ea6bcab4855c23d228aa22e68739e7d84
bhawanjr <bhawanjr@hotmail.com>
2024-02-15 17:43:01 -0300

    refactor: connect to mongodb berfore integrations tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
-import { describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { mongoUrl } from '../../../env'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async() => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
