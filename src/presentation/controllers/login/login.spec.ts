cb1ec7386f6beaafdf53ea804e95f2a019b01a09
bhawanjr <bhawanjr@hotmail.com>
2024-02-19 08:42:46 -0300

    test: ensure LoginController returns 401 if credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vitest } from "vitest"
 import { LoginController } from "./login"
 import { HttpRequest, HttpResponse } from "../../protocols"
-import { badRequest, serverError } from "../../helpers/http-helper"
+import { badRequest, serverError, unauthorized } from "../../helpers/http-helper"
 import { InvalidParamError, MissingParamError } from "../../errors"
 import { EmailValidator } from "../signup/signup-protocols"
 import { Authentication } from "../../../domain/usecases/authentication"
@@ -100,5 +100,13 @@ describe('Login Controller', () => {
     const authSpy = vitest.spyOn(authenticationStub, 'auth')
     await sut.handle(makeFakeRequest())
     expect(authSpy).toHaveBeenLastCalledWith('any_email@mail.com', 'any_password')
+  })
+
+  test('Should return 401 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut()
+    //@ts-ignore
+    vitest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized())
   })
 })
