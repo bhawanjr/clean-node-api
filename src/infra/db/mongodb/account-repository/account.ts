7ff87be1eddeeeb70291b657d8762ff3294be6cd
bhawanjr <bhawanjr@hotmail.com>
2024-02-19 05:58:51 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,7 +3,7 @@ import { AddAccountModel } from "../../../../domain/usecases/add-account"
 import { AccountModel } from "../../../../domain/models/account"
 import { MongoHelper } from "../helpers/mongo-helper"
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository {  
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const newId = (await accountCollection.insertOne(accountData)).insertedId.toString()
