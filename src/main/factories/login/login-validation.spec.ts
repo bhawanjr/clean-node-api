8e24dccd4cf0878e1d423e8f04674a4348d65149
bhawanjr <bhawanjr@hotmail.com>
2024-02-20 12:00:37 -0300

    refactor: group validation imports in a single file

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -1,9 +1,7 @@
 import { describe, expect, test, vitest } from "vitest"
 import { makeLoginValidation } from './login-validation'
-import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite"
-import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation"
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from "../../../presentation/helpers/validators"
 import { Validation } from "../../../presentation/protocols/validation"
-import { EmailValidation } from "../../../presentation/helpers/validators/email-validation"
 import { EmailValidator } from "../../../presentation/protocols/email-validator"
 
 vitest.mock('../../../presentation/helpers/validators/validation-composite')
--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,7 @@
 import { describe, expect, test, vitest } from "vitest"
+import { ValidationComposite, CompareFieldsValidation, EmailValidation, RequiredFieldValidation } from "../../../presentation/helpers/validators"
 import { makeSignupValidation } from './signup-validation'
-import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite"
-import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation"
 import { Validation } from "../../../presentation/protocols/validation"
-import { CompareFieldsValidation } from "../../../presentation/helpers/validators/compare-fields-validation"
-import { EmailValidation } from "../../../presentation/helpers/validators/email-validation"
 import { EmailValidator } from "../../../presentation/protocols/email-validator"
 
 vitest.mock('../../../presentation/helpers/validators/validation-composite')
