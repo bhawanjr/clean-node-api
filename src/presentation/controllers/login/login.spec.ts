e945d5098709455fbbcca8f2592ff4880c39a499
bhawanjr <bhawanjr@hotmail.com>
2024-02-20 11:49:23 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vitest } from "vitest"
 import { LoginController } from "./login"
 import { HttpRequest, Authentication, Validation } from "./login-protocols"
-import { badRequest, ok, serverError, unauthorized } from "../../helpers/http-helper"
+import { badRequest, ok, serverError, unauthorized } from "../../helpers/http/http-helper"
 import { MissingParamError } from "../../errors"
 
 const makeAuthentication = (): Authentication => {
