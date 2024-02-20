9126857ff8d7d4c78fc488e5010a7eda0a404f5e
bhawanjr <bhawanjr@hotmail.com>
2024-02-20 17:22:58 -0300

    refactor: move sut creation to a factory method

--- src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
@@ -10,23 +10,26 @@ vitest.mock('jsonwebtoken', () => {
   }
 })
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
 
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = vitest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   test('Should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
 
   test('Should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     vitest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error()
     })
