a04b4404db52de7b1fa1a2637240bf1e08c8b1eb
bhawanjr <bhawanjr@hotmail.com>
2024-02-20 16:36:13 -0300

    test: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -5,7 +5,8 @@ import { BcryptAdapter } from './bcrypt-adapter'
 vitest.mock('bcrypt', () => {
   return {
     default: {
-      hash: () => 'hashed_value'
+      hash: () => 'hashed_value',
+      compare: () => true      
     }
   }
 })
@@ -25,14 +26,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('BCrypt Adapter', () => {
-  test('Should call bcrypt with correct values', async () => {
+  test('Should call hash with correct values', async () => {
     const { sut, salt } = makeSut()
     const hashSpy = vitest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should returns a hash on success', async () => {
+  test('Should returns a valid hash on hash success', async () => {
     const { sut } = makeSut()
     const hash = await sut.hash('any_value')
     expect(hash).toBe('hashed_value')
@@ -45,5 +46,12 @@ describe('BCrypt Adapter', () => {
     )
     const promise = sut.hash('any_value')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call compare with correct values', async () => {
+    const { sut, salt } = makeSut()
+    const compareSpy = vitest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 })
