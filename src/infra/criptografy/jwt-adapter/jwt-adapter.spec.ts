a27255392d36de752a3f27c6354f3d987a5113cd
bhawanjr <bhawanjr@hotmail.com>
2024-02-20 17:17:29 -0300

    test: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
@@ -2,11 +2,26 @@ import { describe, expect, test, vitest } from "vitest"
 import jwt from "jsonwebtoken"
 import { JwtAdapter } from "./jwt-adapter"
 
+vitest.mock('jsonwebtoken', () => {
+  return {
+    default: {
+      sign: () => 'any_token'  
+    }
+  }
+})
+
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = vitest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
