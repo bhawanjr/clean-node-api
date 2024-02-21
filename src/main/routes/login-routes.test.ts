51bb087a4728c4472e43811a32a6cf1c1e5855fd
bhawanjr <bhawanjr@hotmail.com>
2024-02-21 09:42:51 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,11 @@ import { afterAll, beforeAll, beforeEach, describe, test } from 'vitest'
 import request from 'supertest'
 import app from '../config/app'
 import env from '../config/env'
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -14,7 +18,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -29,6 +33,24 @@ describe('Login Routes', () => {
           passwordConfirmation: '123'
         })
         .expect(201)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 201 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Nicolas',
+        email: 'nicolas@email.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'nicolas@email.com',
+          password: '123'
+        })
+        .expect(200)
     })
   })
 })
