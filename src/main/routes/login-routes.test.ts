a7f1ddf81f271f4a7a4a5c4dbd8ff41e7749cc41
bhawanjr <bhawanjr@hotmail.com>
2024-02-21 09:46:53 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -53,4 +53,14 @@ describe('Login Routes', () => {
         .expect(200)
     })
   })
+  
+  test('Should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'nicolas@email.com',
+        password: '123'
+      })
+      .expect(401)
+  })
 })
