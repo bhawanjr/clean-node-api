bc2b79ab80d9a3749fcd3d3a891159101274bc6c
bhawanjr <bhawanjr@hotmail.com>
2024-02-23 12:13:48 -0300

    chore: add log on server start to test

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,10 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
+console.log('PRECISO TIRAR ESTE LOG: ')
+console.log(env.mongoUrl)
+console.log('-'.repeat(100))
+
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
