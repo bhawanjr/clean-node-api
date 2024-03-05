78a2420df9419fef505abc3b16a539948425bb9f
bhawanjr <bhawanjr@hotmail.com>
2024-03-05 11:31:39 -0300

    test: ensure save survey result route returns 201 on sucess

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,6 +1,6 @@
 import { SaveSurveyResultController } from './save-survey-result-controller'
 import { HttpRequest, LoadSurveyById, SurveyModel, SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel } from './save-survey-result-controller.protocols'
-import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { created, forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 import MockDate from 'mockdate'
 
@@ -129,9 +129,9 @@ describe('SeveSurveyResult Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
-  test('Should return 200 on success', async () => {
+  test('Should return 201 on success', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse).toEqual(ok(makeFakeSurveyResult()))
+    expect(httpResponse).toEqual(created(makeFakeSurveyResult()))
   })
 })
