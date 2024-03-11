6bd3e63d5ef663ac719139b9afe8d5b00666e189
bhawanjr <bhawanjr@hotmail.com>
2024-03-11 15:17:00 -0300

    refactor: group files to a folder

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,4 +1,4 @@
-import { SaveSurveyResultRepository } from '@/data/usecases/survey-result/db-save-survey-result-protocols'
+import { SaveSurveyResultRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { mockSurveyResultModel } from '@/domain/test'
