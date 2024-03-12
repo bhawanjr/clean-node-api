a45995c3c02ec4cf2161a39780e23bb6775cd280
bhawanjr <bhawanjr@hotmail.com>
2024-03-12 17:45:04 -0300

    test: ensure SaveSurveyMongoRepository retuns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -175,8 +175,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository, 
         answers: '$answers'
       })
       .build()
-
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
-    return surveyResult?.length ? surveyResult[0] as SurveyResultModel : null
+    return surveyResult.length ? surveyResult[0] as SurveyResultModel : null
   }
 }
